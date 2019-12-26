9d8ed18120a49620a62c4fa9157228125cd49aa0
WrinkleYourPizzas <59059371+WrinkleYourPizzas@users.noreply.github.com>
2019-12-25 19:37:07 -0500

    Update Physics.java

--- Physics.java
+++ Physics.java
@@ -1,111 +1,80 @@
 import javax.swing.*;
+
 import java.awt.*;
 import java.awt.event.*;
-import java.util.*;
-import java.util.concurrent.ThreadLocalRandom;
 
-public class Physics implements KeyListener {	//KeyListener is like ActionListener but for keyboard
+public class mainMenu implements KeyListener {	//KeyListener is like ActionListener but for keyboard
 	public static final int height = 600;	//Window dimensions
 	public static final int width = 900;
 
-	//Put any and all objects you create into an ArrayList, the whiteboard method will draw their contents onto the panel
-	public static ArrayList<PhysicsObject> physicsObjectList = new ArrayList<PhysicsObject>();	//All physics objects	
-	public static ArrayList<Platform> platformList = new ArrayList<Platform>();	//All platform objects
-	public static ArrayList<Weapon> weaponList = new ArrayList<Weapon>();	//All weapon objects
+	public static boolean start = false;
+
+	public static int selection = 1;
+		
+	JFrame frame;	
+	JPanel panel = new JPanel();	
 	
-	Image backgroundImage  = Toolkit.getDefaultToolkit().createImage("better.jpg").getScaledInstance(width, height,java.awt.Image.SCALE_SMOOTH);
-
-	JFrame frame;
-	JPanel panel = new whiteboard();	//whiteboard is a method which creates a panel that you can "draw" objects onto
-
-	public Physics() {
+	public mainMenu() {
 		frame = new JFrame("Super Smash");	//Frame stuff
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(width, height);
 		frame.setResizable(false);
-		frame.addKeyListener(this);		
+		frame.addKeyListener(this);
 		
 		panel.setLayout(new BorderLayout());	
-				
-		physicsObjectList.add(new PhysicsObject("yoshi.png", "sword.png", ThreadLocalRandom.current().nextInt(100, 300 + 1), 100, 30, 40));	//Adds the two blocks ArrayList
-		physicsObjectList.add(new PhysicsObject("yoshi.png", "sword.png", ThreadLocalRandom.current().nextInt(550, 750 + 1), 100, 30, 40));
 		
-		platformList.add(new Platform(400, 92 ,101, 5));
-		platformList.add(new Platform(280, 170 ,102, 5));
-		platformList.add(new Platform(519, 170 ,102, 5));
-		platformList.add(new Platform(400, 245 ,101, 5));
-		platformList.add(new Platform(158, 245 ,107, 5));
-		platformList.add(new Platform(637, 245 ,105, 5));
-		platformList.add(new Platform(90, 315 ,710, 15));
+		putImage("smashBrosEntry.png");
 		
-		for(int i=0; i<physicsObjectList.size(); i++) 
-			weaponList.add(physicsObjectList.get(i).getweapon());
-
 		frame.add(panel);
-
+		
 		frame.setLocationRelativeTo(null);	//Make the frame visible
-		frame.setVisible(true);
-
-		Thread animationThread = new Thread(new Runnable() {	//The main loop
-			public void run() {	
-				while (true) {	
-					frame.repaint();	//Refresh frame and panel
-					panel.repaint();
-					try {Thread.sleep(12);} catch (Exception ex) {}	//10 millisecond delay between each refresh
-				}
-			}
-		});	
-		animationThread.start();	//Start the main loop
-	}
-
-	public void keyTyped(KeyEvent e) {}		//KeyListener is an interface so must implement all empty methods, this one is just useless
-
-	public void keyPressed(KeyEvent e) {	//When the keys are pressed (when they're released is the method after this one)
-		//Arrow keys for object1
-		if(e.getKeyCode() == KeyEvent.VK_RIGHT) physicsObjectList.get(0).moveX(4);	// "VK_RIGHT" --> Right arrow 
-		else if(e.getKeyCode() == KeyEvent.VK_LEFT) physicsObjectList.get(0).moveX(-4);	//Get the first object from physicsObjectList, positive X moves right
-
-		if(e.getKeyCode() == KeyEvent.VK_UP) {
-			if(!physicsObjectList.get(0).fallingStatus()) physicsObjectList.get(0).moveY(-10);	//Negative Y moves up
-		}
-		
-		if(e.getKeyCode() == KeyEvent.VK_COMMA) 
-			physicsObjectList.get(0).swingWeapon();
-
-		//WASD for object2
-		if(e.getKeyCode() == KeyEvent.VK_D) physicsObjectList.get(1).moveX(4);	//"VK_*LETTER*" --> "D" key
-		else if(e.getKeyCode() == KeyEvent.VK_A) physicsObjectList.get(1).moveX(-4);	//physicsObjectList.get(1): gets second obejct (the only two are the boxes)
-
-		if(e.getKeyCode() == KeyEvent.VK_W) {
-			if(!physicsObjectList.get(1).fallingStatus()) physicsObjectList.get(1).moveY(-10);
-		}
-		
-		if(e.getKeyCode() == KeyEvent.VK_X) 
-			physicsObjectList.get(1).swingWeapon();
-	}
-
-	public void keyReleased(KeyEvent e) {	//When the keys are released
-		if(e.getKeyCode() == KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_RIGHT) physicsObjectList.get(0).moveX(0); //Object1
-		if(e.getKeyCode() == KeyEvent.VK_A || e.getKeyCode() == KeyEvent.VK_D) physicsObjectList.get(1).moveX(0); //Object2
-	}
-
-	public class whiteboard extends JPanel {	//Make a new JPanel that you can draw objects onto (Can't draw stuff anywhere you want onto normal JPanels)
-		public void paintComponent(Graphics g) {
-			super.paintComponent(g);	//Call paintComponent from the overlord JPanel
-			g.drawImage(backgroundImage, 0, 0, null);
-
-			for(int i=0; i<physicsObjectList.size(); i++) //Draws all the obejcts from physicsObjectList
-				physicsObjectList.get(i).draw(g);
-			
-//			for(int i=0; i<platformList.size(); i++) //Draws all the obejcts from physicsObjectList
-//				platformList.get(i).draw(g);
-			
-			for(int j=0; j<weaponList.size(); j++)	//Draw contents in weaponList
-				weaponList.get(j).draw(g);
-		}
+		frame.setVisible(true);	
 	}
 	
+	public void selectionMenu(int selection) {
+		clearAll();
+		putImage("smashMenu" + selection + ".png");
+	}
+	
+	public void putImage(String img) {
+		ImageIcon icon = new ImageIcon(img);
+		Image image = icon.getImage().getScaledInstance(width-20, height-40, java.awt.Image.SCALE_SMOOTH);
+		JLabel pic = new JLabel(new ImageIcon(image));
+		panel.add(pic);
+	}
+	
+	public void clearAll() {
+		panel.removeAll();
+		panel.revalidate();
+		panel.repaint();
+	}
+
+	public void keyTyped(KeyEvent e) {}	//KeyListener is an interface so must implement all empty methods, this one is just useless
+
+	public void keyPressed(KeyEvent e) {	//When the keys are pressed (when they're released is the method after this one)
+		if(e.getKeyCode() == KeyEvent.VK_SPACE) {
+			if(!start) {
+				selectionMenu(1); 
+				start = true;
+			}
+		}
+		if(e.getKeyCode() == KeyEvent.VK_DOWN && start && selection < 5) {
+			selection++;
+			selectionMenu(selection);
+		}
+		if(e.getKeyCode() == KeyEvent.VK_UP && start && selection > 1) {
+			selection--;
+			selectionMenu(selection);
+		}
+		if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+			clearAll();
+			if(selection == 1) new Physics();
+		}
+	}
+
+	public void keyReleased(KeyEvent e) {}
+
 	public static void main(String[] args) {	//Call the graphics constructor
-		new Physics();
+		new mainMenu();
 	}
 }
