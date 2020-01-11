9f32c89af7c7982217f10ebf6280ae1ac45a760d
WrinkleYourPizzas <59059371+WrinkleYourPizzas@users.noreply.github.com>
2020-01-11 12:20:13 -0500

    Update Physics.java

--- Physics.java
+++ Physics.java
@@ -1,6 +1,9 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -8,11 +11,13 @@ public class Physics implements KeyListener {	//KeyListener is like ActionListen
 	public static final int height = 600;	//Window dimensions
 	public static final int width = 900;
 
-	//Put any and all objects you create into an ArrayList, the canvas method will draw their contents onto the panel
+	//Put any and all objects you create into an ArrayList, the Canvas method will draw their contents onto the panel
 	public static ArrayList<PhysicsObject> physicsObjectList = new ArrayList<PhysicsObject>();	//All physics objects	
 	public static ArrayList<Platform> platformList = new ArrayList<Platform>();	//All platform objects
 	public static ArrayList<MeleeWeapon> weaponList = new ArrayList<MeleeWeapon>();	//All weapon objects
 	public static ArrayList<ProjectileWeapon> projectileList = new ArrayList<ProjectileWeapon>(); //All projectiles
+	
+	public static HashMap<String, BufferedImage> imageMap = new HashMap<String, BufferedImage>();
 
 	public static boolean paused = false;
 	public static boolean quit = false;
@@ -21,13 +26,9 @@ public class Physics implements KeyListener {	//KeyListener is like ActionListen
 	public static boolean P2IsShooting = false;
 
 	public static Image backgroundImage = Toolkit.getDefaultToolkit().createImage("better.jpg").getScaledInstance(width, height,java.awt.Image.SCALE_SMOOTH);
-	public static Image fireball = Toolkit.getDefaultToolkit().createImage("fireball.png");
-	public static Image sword = Toolkit.getDefaultToolkit().createImage("sword.png");
-	public static Image hand = Toolkit.getDefaultToolkit().createImage("hand.png");
-	public static Image steam = Toolkit.getDefaultToolkit().createImage("steam.png");
 
 	public static JFrame frame;
-	public static JPanel panel = new canvas();	//canvas is a method which creates a panel that you can "draw" objects onto
+	public static JPanel panel = new Canvas();	//Canvas is a method which creates a panel that you can "draw" objects onto
 
 	public Physics() {
 		frame = new JFrame("Super Smash");	//Frame stuff
@@ -49,8 +50,16 @@ public class Physics implements KeyListener {	//KeyListener is like ActionListen
 		platformList.add(new Platform(637, 245 ,105, 15, false, true));
 		platformList.add(new Platform(90, 315 ,710, 25, false, true));
 		
-		platformList.add(new Platform(30, 350, 60, 25, true, true));
-		platformList.add(new Platform(800, 350, 60, 25, true, false));
+		platformList.add(new Platform(30, 330, 60, 25, true, true));
+		platformList.add(new Platform(800, 330, 60, 25, true, false));
+		
+		try {
+			imageMap.put("fireball", ImageIO.read(new File("fireball.png")));
+			imageMap.put("sword", ImageIO.read(new File("sword.png")));
+			imageMap.put("hand", ImageIO.read(new File("hand.png")));
+			imageMap.put("handFlipped", toBufferedImage(flip(ImageIO.read(new File("hand.png")))));
+			imageMap.put("steam", ImageIO.read(new File("steam.png")));
+		} catch(IOException e) {}
 
 		frame.add(panel);
 
@@ -67,7 +76,7 @@ public class Physics implements KeyListener {	//KeyListener is like ActionListen
 			}
 		});
 		Thread animationThread = new Thread(new Runnable() {	//The main loop
-			public void run() {	
+			public void run() {
 				while(!quit) {
 					while (!paused) {
 						if(P1IsShooting) physicsObjectList.get(0).swingWeapon();
@@ -87,6 +96,25 @@ public class Physics implements KeyListener {	//KeyListener is like ActionListen
 	public static void closeAll() {
 		quit = true;
 		paused = true;
+	}
+	
+	public static Image flip(BufferedImage original) {	//Flip image in parameter, then return it
+		BufferedImage img = new BufferedImage(original.getWidth(), original.getHeight(), BufferedImage.TYPE_INT_ARGB);
+		for(int i = original.getWidth()-1; i>0; i--)
+			for(int j=0; j<original.getHeight(); j++)
+				img.setRGB(original.getWidth()-i, j, original.getRGB(i, j));
+		return img;
+	}
+
+	public static BufferedImage toBufferedImage(Image img) {	//Convert Image into BufferedImage, returns Image
+		if(img instanceof BufferedImage) return (BufferedImage) img;
+
+		BufferedImage temp = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
+		Graphics2D graphics = temp.createGraphics();
+		graphics.drawImage(img, 0, 0, null);
+		graphics.dispose();
+
+		return temp;
 	}
 
 	public void clearAll() {	//Clear all components from panel
@@ -127,7 +155,7 @@ public class Physics implements KeyListener {	//KeyListener is like ActionListen
 		if(e.getKeyCode() == KeyEvent.VK_X) P2IsShooting = false;
 	}
 
-	public static class canvas extends JPanel {	//Make a new JPanel which unlike regular JPanels you can draw objects onto 
+	public static class Canvas extends JPanel {	//Make a new JPanel which unlike regular JPanels you can draw objects onto 
 		public void paintComponent(Graphics g) {
 			super.paintComponent(g);	//Call paintComponent from the overlord JPanel
 			g.drawImage(backgroundImage, 0, 0, null);
@@ -142,7 +170,7 @@ public class Physics implements KeyListener {	//KeyListener is like ActionListen
 					for(int j=0; j<physicsObjectList.size(); j++) {	//Checks if projectile has hit one of the player objects
 						for(int k=0; k<projectileList.size(); k++) {
 							if(Math.abs(projectileList.get(k).getX()-physicsObjectList.get(j).getX()) <= 5 && Math.abs(projectileList.get(k).getY()-physicsObjectList.get(j).getY()) <= 15 && projectileList.get(k).getOwner() != physicsObjectList.get(j)) {
-								physicsObjectList.get(Math.abs(j-1)).dealDamage(projectileList.get(k).getDamage(), -physicsObjectList.get(j).getOrientation(), physicsObjectList.get(j));
+								physicsObjectList.get(Math.abs(j-1)).dealDamage(projectileList.get(k).getDamage(), physicsObjectList.get(j));
 								projectileList.remove(k);
 							}
 						}
